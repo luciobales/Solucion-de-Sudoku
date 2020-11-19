df38be973489337a1d8eabd7b1db37168e5f37bc
Lucio Balestieri <lbales93@gmail.com>
2020-11-18 22:37:54 -0300

    Se rehace la funcion de insertar en matrix

--- Sudoku.hs
+++ Sudoku.hs
@@ -4,13 +4,13 @@ columnas = 3
 espacios = filas * columnas
 matrizVacia = crearMatriz filas columnas
 
---sudoku1 = insertarEnMatriz [(2,0,0)] matrizVacia
---sudoku2 = insertarEnMatriz [(5,0,1)] sudoku1
---sudoku3 = insertarEnMatriz [(6,1,1)] sudoku2
---sudoku4 = insertarEnMatriz [(8,1,2)] sudoku3
---sudoku5 = insertarEnMatriz [(1,2,0)] sudoku4
---sudoku6 = insertarEnMatriz [(4,2,1)] sudoku5
---sudoku  = sudoku6
+sudoku1 = insertarEnMatriz [(2,1,1)] matrizVacia
+sudoku2 = insertarEnMatriz [(5,1,2)] sudoku1
+sudoku3 = insertarEnMatriz [(6,2,2)] sudoku2
+sudoku4 = insertarEnMatriz [(8,2,3)] sudoku3
+sudoku5 = insertarEnMatriz [(1,3,1)] sudoku4
+sudoku6 = insertarEnMatriz [(4,3,2)] sudoku5
+sudoku  = sudoku6
 
 menu :: IO()
 menu = do putStrLn "\n--- --- --- --- --- --- Resolvedor de Sudoku --- --- --- --- --- ---\n"
@@ -24,12 +24,13 @@ menu = do putStrLn "\n--- --- --- --- --- --- Resolvedor de Sudoku --- --- --- -
           putStr "Columna: "
           opc <- getLine 
           let columna = read opc :: Integer
-          let lista1 = ingresarNumeros num fila columna lista
+          --let lista1 = ingresarNumeros num fila columna lista
+          let lista1 = insertarEnMatriz [(num,fila,columna)] matrizVacia
           putStr "Desea ingresar otro numero? (s/n):  "
           rta <-getLine
           if rta == "s"
            then menu2 lista1
-           else putStrLn (show lista1) --"Comenzemos!"
+           else menu3 lista1--putStrLn (show lista1)--menu3 lista1 --"Comenzemos!"
 
 menu2 :: [(Integer,Integer,Integer)] -> IO()
 menu2 lista1 = do putStr "Numero: "
@@ -41,19 +42,27 @@ menu2 lista1 = do putStr "Numero: "
                   putStr "Columna: "
                   opc <- getLine 
                   let columna = read opc :: Integer
-                  let lista2 = ingresarNumeros num fila columna lista1
+                  --let lista2 = ingresarNumeros num fila columna lista1
+                  let lista2 = insertarEnMatriz [(num,fila,columna)] lista1
                   putStr "Desea ingresar otro numero? (s/n):  "
                   rta <-getLine 
                   if rta == "s"
                    then menu2 lista2
-                   else putStrLn (show lista2) --"Comenzemos!"
+                   else menu3 lista2--putStrLn (show lista2)--menu3 lista2 --"Comenzemos!"
 
+menu3 :: [(Integer,Integer,Integer)] -> IO() 
+menu3 lista2 = do putStr "\n--- --- --- --- --- --- Tablero a resolver --- --- --- --- --- ---\n"
+                  mostrarMatriz lista2 
+                  putStrLn "Presione cualquier tecla para continuar"
+                  rta <-getLine
+                  putStrLn "El resultado es... "
+                  
 -- Ver de crear un menu3 para mostrar sudoku y otras opcs
 
 -- Funcion que añade numeros ingresados del menu a la lista
 ingresarNumeros :: Integer -> Integer -> Integer -> [(Integer,Integer,Integer)] -> [(Integer,Integer,Integer)]
 ingresarNumeros num fila colu []     = [(num, fila, colu)]
-ingresarNumeros num fila colu (y:ys) = y:ingresarNumeros num fila colu ys
+ingresarNumeros num fila colu (y:ys) = y:ingresarNumeros num fila colu ys --Si ingreso 2 veces la misma posicion se rompe
 
 
 -- Funcion que crea una matriz vacia
@@ -62,16 +71,16 @@ crearMatriz 1 1 = [(0,1,1)]
 crearMatriz fila columna = if columna == 1
                             then (crearMatriz (fila-1) (columna+2)) ++ [(0,fila,columna)]
                             else (crearMatriz fila (columna-1)) ++ [(0,fila,columna)]
---crearMatriz fila columna = (crearMatriz fila (columna-1)) ++ [(0,fila,columna)]
 
 
 -- -- Funcion que inserta numeros de la lista a la matriz
--- insertarEnMatriz :: [(Integer,Integer,Integer)] -> [(Integer,Integer,Integer)] -> [(Integer,Integer,Integer)]
--- insertarEnMatriz [(num, fila, colu)] ((x,y,z):xs) = if fila == x
---                                                      then if colu == y
---                                                            then (num, fila, colu) : xs
---                                                            else (x,y,z) : insertarEnMatriz [(num, fila, colu)] xs
-
+insertarEnMatriz :: [(Integer,Integer,Integer)] -> [(Integer,Integer,Integer)] -> [(Integer,Integer,Integer)]
+insertarEnMatriz [(num, fila, colu)] [] = [(num, fila, colu)]
+insertarEnMatriz [(num, fila, colu)] ((x,y,z):xs) = if fila == y
+                                                      then if colu == z
+                                                            then (num, fila, colu) : xs
+                                                            else (x,y,z) : insertarEnMatriz [(num, fila, colu)] xs
+                                                      else (x,y,z) : insertarEnMatriz [(num, fila, colu)] xs
 
 
 -- Funcion que imprime por pantalla la matriz
